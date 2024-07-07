491ff520a975b83cf7dfcf5cc2d1bbde107d3fb5
LakshaySanhotra <lakshaysanhotra1@gmail.com>
2024-07-07 23:43:14 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
       <div className="container">
         <a className="navbar-brand text-light" href="#">ReactJS</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <i className="fa-solid fa-bars text-light" style={{fontSize: '25px'}}></i>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0 text-center">
